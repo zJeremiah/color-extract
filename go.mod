3995308c581912b92eeda01e318aa1d3f82f9a0e
Jeremiah Z <z.jeremiah@ymail.com>
2021-08-23 07:58:47 -0600

    used base64 to embed image into html file output

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module github.com/zJeremiah/color-extract
 go 1.16
 
 require (
+	github.com/fogleman/gg v1.3.0
 	github.com/go-playground/colors v1.2.0
-	golang.org/x/image v0.0.0-20210622092929-e6eecd499c2c
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jbsmith7741/uri v0.4.2 // indirect
+	github.com/pcelvng/task-tools v0.17.3
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 
 	"github.com/go-playground/colors"
+	"github.com/pcelvng/task-tools/file"
 	_ "golang.org/x/image/webp"
 )
 
@@ -24,13 +29,17 @@ type Pixel struct {
 	A int
 }
 
+// Stats is the definition of a color in the image
 type Stats struct {
+	ID string // the id of the color
 	Pixel
 	Count   int
 	Percent float64
 }
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
 	htmlHeader := []byte(`<!DOCTYPE html>
 <html>
 <head>
@@ -61,8 +70,9 @@ func main() {
 <div>
 `)
 
-	htmlPixels := `<div><img src="%s"></div>
-<div style='font-size: larger;margin:20px'>pixels: %d size (%d x %d) Colors: %d</div>
+	htmlPixels := `<div><img src="%s"></div><p style="page-break-before: always"></p>
+<div style='font-size: larger;margin:20px'>pixel count: %d pixels size (%d x %d) Colors: %d</div>
+<div style='font-size: larger;margin:20px'>%s</div>
 `
 	htmlTable := []byte(`
 	<table class="table" style="width:70%;">
@@ -77,21 +87,19 @@ func main() {
 <tbody>
 `)
 
-	htmlColors := `
+	colorRowHtml := `
   <tr>
-    <td style="text-align:center">%d</td>
+    <td style="text-align:center">%s</td>
     <td style="background-color:%s;"></td>
-	<td>%d</td>
-	<td>%s</td>
-	<td>%s</td>
-	<td>%.2f%%</td>
+    <td>%d</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%.2f%%</td>
   </tr>
 `
 
 	flag.Parse()
 	input := flag.Arg(0)
-	row := flag.Arg(1)
-	rowNum, _ := strconv.Atoi(row)
 	colorCount := make(map[Pixel]int)
 	imgName := input
 
@@ -108,39 +116,54 @@ func main() {
 	// loops though all the pixels to get the color of each one
 	// and to count each of the colors found
 	bounds := m.Bounds()
-	outputRow := make([]Pixel, 0)
+	total := bounds.Max.X * bounds.Max.Y
+	output := make([][]Pixel, bounds.Max.Y)
+
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			// get the rgba values of the pixel
 			r, g, b, a := m.At(x, y).RGBA()
+			// convert the mulitplied rgba values to an actual pixel
 			p := rgbaToPixel(r, g, b, a)
-
-			if row != "" && y == rowNum {
-				outputRow = append(outputRow, p)
-			}
-
+			// add the pixel to the matrix
+			output[y] = append(output[y], p)
+			// add the pixel to the color count
 			colorCount[p]++
 		}
 	}
 
 	buf := bytes.NewBuffer(htmlHeader)
 
-	total := bounds.Max.X * bounds.Max.Y
 	buf.Write([]byte(
 		fmt.Sprintf(htmlPixels,
-			imgName,
+			base64PNG(imgName),
 			total,
 			bounds.Max.X,
 			bounds.Max.Y,
-			len(colorCount)),
+			len(colorCount),
+			sizeStr(bounds.Max)),
 	))
 
+	colorList := []string{"Σ", "∆", "@", "#", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "U", "V", "W", "X", "Y", "Z"}
 	list := make([]Stats, 0)
+
+	i := 0
 	for k, v := range colorCount {
+		var c string
+		// if there are more colors than the color list, just use the number (can't make pattern)
+		if i > len(colorList)-1 {
+			c = strconv.Itoa(i)
+		} else {
+			c = colorList[i]
+		}
+
 		list = append(list, Stats{
+			ID:      c,
 			Pixel:   k,
 			Count:   v,
-			Percent: float64(v) / float64(total) * 100,
+			Percent: float64(v) / float64(total) * 100.0,
 		})
+		i++
 	}
 
 	buf.Write(htmlTable)
@@ -151,45 +174,109 @@ func main() {
 	})
 
 	// loop though the stats list and build a html table row for each item
-	for i, l := range list {
-		rgb := fmt.Sprintf("rgb(%d,%d,%d)", l.R, l.G, l.B)
+	for i := range list {
+		rgb := fmt.Sprintf("rgb(%d,%d,%d)", list[i].R, list[i].G, list[i].B)
 		color, err := colors.Parse(rgb)
 		if err != nil {
 			log.Println("could not parse color", rgb)
 		}
-		s := fmt.Sprintf(htmlColors,
-			i,
+
+		if i > len(colorList)-1 {
+			list[i].ID = strconv.Itoa(i)
+		} else {
+			list[i].ID = colorList[i]
+		}
+
+		id := list[i].ID
+		if id == "Σ" {
+			id = "&#931;"
+		}
+
+		if id == "∆" {
+			id = "	&#8710;"
+		}
+
+		s := fmt.Sprintf(colorRowHtml,
+			id,
 			rgb,
-			l.Count,
+			list[i].Count,
 			color.ToHEX(),
 			rgb,
-			l.Percent)
+			list[i].Percent)
 
 		buf.Write([]byte(s))
 	}
 
 	buf.Write([]byte("</tbody>\n</table>\n"))
-
-	// Added a 2nd Arg to allow the printing of a list of the colors (by number)
-	// in a selected row
-	if len(outputRow) > 0 {
-		buf.Write([]byte("<br><hr><div>Row:" + row + " Output Colors</div>\n<div>\n"))
-		for _, v := range outputRow {
-			for i, b := range list {
-				if b.Pixel == v {
-					buf.Write([]byte(fmt.Sprintf("%d, ", i)))
-					break
-				}
-			}
-		}
-		buf.Write([]byte("</div>\n"))
-	}
-
 	buf.Write([]byte("</div>\n</body>\n</html>\n"))
 	fmt.Print(buf.String())
+
+	// write out a pattern.csv file that can be use to generate a pattern image
+	names := strings.Split(imgName, ".")
+	grid := Grid{
+		Codes: make(map[Point]Position),
+		Name:  names[0],
+	}
+
+	os.Remove("pattern.csv")
+	if len(list) <= len(colorList) {
+		c, _ := file.NewWriter("pattern.csv", file.NewOptions())
+		for iy, y := range output {
+			//c.Write([]byte(""))
+			for ix, x := range y {
+				for _, s := range list {
+					if s.Pixel == x {
+						c.Write([]byte(s.ID))
+						grid.Set(ix, iy, s.ID)
+						break
+					}
+				}
+			}
+			c.Write([]byte("\n"))
+		}
+		c.Close()
+	} else {
+		log.Println("to many colors to create pattern", len(list), "limit", len(colorList))
+	}
+
+	newImageGrid(bounds.Max.X*30, bounds.Max.Y*30, grid)
 }
 
 // img.At(x, y).RGBA() returns four uint32 values; we want a Pixel
 func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
 	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+func sizeStr(p image.Point) string {
+	mmX := p.X * 2
+	mmY := p.Y * 2
+	inX := float64(mmX) / 25.4
+	inY := float64(mmY) / 25.4
+
+	return fmt.Sprintf("size in 2mm (%d x %d) size in inches (%.2f, %.2f)", mmX, mmY, inX, inY)
+}
+
+func base64PNG(imgPath string) string {
+	b, err := ioutil.ReadFile(imgPath)
+	if err != nil {
+		log.Fatalln("cannot read image", err)
+	}
+
+	var base64Img string
+
+	mimeType := http.DetectContentType(b)
+
+	// Prepend the appropriate URI scheme header depending
+	// on the MIME type
+	switch mimeType {
+	case "image/jpeg":
+		base64Img += "data:image/jpeg;base64,"
+	case "image/png":
+		base64Img += "data:image/png;base64,"
+	case "imge/webp":
+		base64Img += "data:image/webp;base64,"
+	}
+
+	base64Img += base64.StdEncoding.EncodeToString(b)
+	return base64Img
 }
